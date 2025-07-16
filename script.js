a31147bf6197f6cd31283ff2661a105e15a3f6e0
Khrystyna <penyakkristi@gmail.com>
2025-07-16 21:43:55 +0300

    feature: add Reset Grid button for user interaction

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const createGrid = function (size = 16) {
   containerDiv.innerHTML = "";
   for (let row = 0; row < size; row++) {
     const rowDiv = document.createElement("div");
-    rowDiv.classList.add("row", `row-${row}`);
+    rowDiv.classList.add("row");
     containerDiv.append(rowDiv);
     for (let col = 0; col < size; col++) {
       const colDiv = document.createElement("div");
-      colDiv.classList.add("col", `col-${col}`);
+      colDiv.classList.add("col");
       colDiv.addEventListener("mouseenter", () => {
         colDiv.classList.add("hoovered");
       });
@@ -18,13 +18,21 @@ const createGrid = function (size = 16) {
   }
 };
 
+const btnReset = document.querySelector(".grid__btn-reset");
+const btnEdit = document.querySelector(".grid__btn-edit");
+
 const editGridSize = function () {
-  const btnEdit = document.querySelector(".grid__btn-edit");
-  btnEdit.addEventListener("click", () => {
-    const value = Number(prompt("Enter a number that is less than 100: "));
-    createGrid(value);
+  const value = Number(prompt("Enter a number that is less than 100: "));
+  createGrid(value);
+};
+
+const resetGrid = function () {
+  const allCols = document.querySelectorAll(".col");
+  allCols.forEach((col) => {
+    col.classList.remove("hoovered");
   });
 };
 
 createGrid();
-editGridSize();
+btnEdit.addEventListener("click", editGridSize);
+btnReset.addEventListener("click", resetGrid);
