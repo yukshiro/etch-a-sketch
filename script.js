f9cf1367db444d219dffc7e5eab08f5e727a4928
Khrystyna <penyakkristi@gmail.com>
2025-07-16 21:19:43 +0300

    feature: add grid resize button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const createGrid = function (size) {
+const createGrid = function (size = 16) {
   const containerDiv = document.querySelector(".grid__container");
+  containerDiv.innerHTML = "";
   for (let row = 0; row < size; row++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row", `row-${row}`);
@@ -17,4 +18,13 @@ const createGrid = function (size) {
   }
 };
 
-createGrid(10);
+const editGridSize = function () {
+  const btnEdit = document.querySelector(".grid__btn-edit");
+  btnEdit.addEventListener("click", () => {
+    const value = Number(prompt("Enter a number that is less than 100: "));
+    createGrid(value);
+  });
+};
+
+createGrid();
+editGridSize();
