8ce874847997d507573f6adf5375442cddd963a3
Khrystyna <penyakkristi@gmail.com>
2025-07-16 20:26:26 +0300

    style: add hover effect for grid cells

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 "use strict";
 
-const containerDiv = document.querySelector(".grid__container");
-
 const createGrid = function (size) {
+  const containerDiv = document.querySelector(".grid__container");
   for (let row = 0; row < size; row++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row", `row-${row}`);
@@ -10,6 +9,9 @@ const createGrid = function (size) {
     for (let col = 0; col < size; col++) {
       const colDiv = document.createElement("div");
       colDiv.classList.add("col", `col-${col}`);
+      colDiv.addEventListener("mouseenter", () => {
+        colDiv.classList.add("hoovered");
+      });
       rowDiv.append(colDiv);
     }
   }
